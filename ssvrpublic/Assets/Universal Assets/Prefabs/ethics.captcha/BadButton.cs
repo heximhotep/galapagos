a3cb2792cc359bb02ece5416b0804821ca28bc9f
David Gordon <heximhotep@gmail.com>
2016-12-04 14:08:44 -0500

    removed older apks for size concerns. added ethics capchas (currently sitting in middle of claw room). added body diagram asset, yet to correctly implement. fixed errors with player overlay canvas.

---
+++ ssvrpublic/Assets/Universal Assets/Prefabs/ethics.captcha/BadButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BadButton : MonoBehaviour {
+	public GameObject goodButton;
+
+	private bool readyToClick;
+	private GameObject captchaController;
+	private GameObject player;
+	private PlayerController playerScript;
+	private EthicsCaptcha captchaScript;
+
+	// Use this for initialization
+	void Start () {
+		captchaController = GameObject.Find("CaptchaController");
+		captchaScript = captchaController.GetComponent<EthicsCaptcha>();
+		player = GameObject.Find("player");
+		playerScript = player.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (readyToClick && Input.GetButtonDown("Fire1")) {
+			captchaScript.humanVal -= 1;
+			string terminalName = gameObject.name;
+			int terminalVal = int.Parse(terminalName.Substring(terminalName.Length - 1));
+			print(terminalVal);
+			captchaScript.SayThanks(terminalVal);
+			gameObject.SetActive(false);
+			goodButton.SetActive(false);
+			playerScript.startMovement();
+		}
+		
+	}
+
+	public void Deactivate (){
+		readyToClick = false;
+	}
+
+	public void Activate (){
+		readyToClick = true;
+	}
+
+}
---
+++ ssvrpublic/Assets/Universal Assets/Prefabs/ethics.captcha/GoodButton.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodButton : MonoBehaviour {
+	public GameObject badButton;
+
+	private bool readyToClick;
+	private GameObject captchaController;
+	private GameObject player;
+	private PlayerController playerScript;
+	private EthicsCaptcha captchaScript;
+
+	// Use this for initialization
+	void Start () {
+		captchaController = GameObject.Find("CaptchaController");
+		captchaScript = captchaController.GetComponent<EthicsCaptcha>();
+		player = GameObject.Find("player");
+		playerScript = player.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (readyToClick && Input.GetButtonDown("Fire1")) {
+			captchaScript.humanVal += 1;
+			print(captchaScript.humanVal);
+			string terminalName = gameObject.name;
+			int terminalVal = int.Parse(terminalName.Substring(terminalName.Length - 1));
+			print(terminalVal);
+			captchaScript.SayThanks(terminalVal);
+			gameObject.SetActive(false);
+			badButton.SetActive(false);
+			playerScript.startMovement();
+
+		}
+		
+	}
+
+	public void Deactivate (){
+		readyToClick = false;
+	}
+
+	public void Activate (){
+		readyToClick = true;
+	}
+
+}
